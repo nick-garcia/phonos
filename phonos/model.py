00836a523a032cc8071b24b62142086ced29f125
Nick Garcia <foxpaw@gmail.com>
2018-06-12 18:20:48 -0700

    Added country table. Added initialization of country table.

--- phonos/model.py
+++ phonos/model.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.types import Enum
 from phonos import app
 
+import csv
 import enum
 
 db = SQLAlchemy(app)
@@ -9,6 +9,16 @@ db = SQLAlchemy(app)
 def initialize():
     db.drop_all()
     db.create_all()
+
+    with open('phonos/data/ISO_Country_Codes.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for country in reader:
+            row = Country()
+            row.name = country['Name']
+            row.code = country['Code']
+            db.session.add(row)
+
+    db.session.commit()
 
 class PhoneStatus(enum.Enum):
     active = "Active"
@@ -23,6 +33,14 @@ class PhoneNumber(db.Model):
     type = db.Column(db.String(10), nullable=False)  # (Avaya, Cisco, mobile)
     status = db.Column(db.Enum(PhoneStatus))
 
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50))
 
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(2), nullable=False)
