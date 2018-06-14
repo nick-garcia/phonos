4a77f29ffc3242978b5e5dcccebed9f874d0592e
Nick Garcia <foxpaw@gmail.com>
2018-06-13 18:07:17 -0700

    Added import function for mobile numbers.

--- phonos/util.py
+++ phonos/util.py
@@ -58,16 +58,51 @@ def import_cisco_numbers(file):
     raise NotImplementedError("Importing phone numbers from Cisco has not been implemented yet.")
 
 def import_mobile_numbers(file):
-    pass
+    country_lookup = get_country_lookups()
 
+    records = 0
+    for row in file:
+        lastname, firstname = row['name'].split(', ')
+        person = model.Person.query.filter_by(firstname=firstname, lastname=lastname).first()
+        if not person:
+            person = model.Person(firstname=firstname, lastname=lastname)
 
-CountryLookup = namedtuple("CountryLookup", ["by_code", "by_name"])
+        if len(row["country"]) == 2:
+            country = country_lookup.by_code[row["country"]]
+            country_code = row["country"]
+        else:
+            country = country_lookup.by_name.get(row['country'], 'Unknown')
+            country_code = country_lookup.code_by_name[row['country']]
+
+        parsed_number = phonenumbers.parse(row['number'], country_code)
+        extra = {
+            'carrier' : row['carrier'],
+            'device_type' : row['devicetype']
+        }
+
+        number = model.PhoneNumber(
+            number=phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164),
+            type="mobile",
+            country=country,
+            person=person,
+            extra=extra
+        )
+
+        model.db.session.add(number)
+        records += 1
+
+    model.db.session.commit()
+    return records
+
+CountryLookup = namedtuple("CountryLookup", ["by_code", "by_name", "code_by_name"])
 def get_country_lookups():
     by_code = {}
     by_name = {}
+    code_by_name = {}
 
     for country in model.Country.query:
         by_code[country.code] = country
         by_name[country.name] = country
+        code_by_name[country.name] = country.code
 
-    return CountryLookup(by_code=by_code, by_name=by_name)
+    return CountryLookup(by_code=by_code, by_name=by_name, code_by_name=code_by_name)
