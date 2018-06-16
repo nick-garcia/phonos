7e7fb0899b3bf7a10a087652ea7d45a483b9fef1
Nick Garcia <foxpaw@gmail.com>
2018-06-16 15:03:59 -0700

    Added a UI for uploading files to import.

--- phonos/ui_routes.py
+++ phonos/ui_routes.py
@@ -1,6 +1,19 @@
-from flask import render_template
-from phonos import app
+from flask import render_template, request
+from phonos import app, util
 
 @app.route('/')
+@app.route('/index.html')
 def index_html():
     return render_template('index.html')
+
+@app.route('/import.html', methods=['GET', 'POST'])
+def import_html():
+    imported = 0
+    if request.method == 'POST':
+        csvfile = request.files['csvfile']
+        filetype = request.form['filetype']
+
+        csvfile = [x.decode() for x in csvfile]
+        imported = util.import_numbers(csvfile, filetype)
+        
+    return render_template('import.html', imported=imported)
